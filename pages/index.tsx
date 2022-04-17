b0d3504c2d726235a2e6e6470d85179383b17903
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-04-17 18:05:41 +0900

    fix: 暫定的に修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,6 @@
 import type { NextPage } from 'next'
 
-import Home from 'components/home'
-
-const HomePage: NextPage = () => (
-  <Home
-    idol={{
-      id: 'rinze_morino',
-      name: '杜野凛世',
-      color: '#1da1f2',
-      birth: { month: 10, day: 19 }
-    }}
-    ogpImageUrl=""
-  />
-)
+// TODO: ランダムなアイドルのページに飛ばしたい
+const HomePage: NextPage = () => <div>test</div>
 
 export default HomePage
