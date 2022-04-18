327614808c7e046d03683d4895df048ea02b9503
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-04-18 20:44:45 +0900

    feat: OGP画像生成を実装

--- libs/ogp.ts
+++ libs/ogp.ts
@@ -8,6 +8,71 @@ import { calcSecondsToBirthday, createJstDate } from './date'
 import { verification } from './hash'
 
 /**
+ * Cloudinary用に文字列をエンコード
+ * @param text 文字列
+ * @returns エンコードされた文字列
+ */
+export const encodeForCloudinary = (text: string) => {
+  return encodeURIComponent(
+    text.replace(/\,/g, '%2C').replace(/\//g, '%2F').replace(/!/g, '%21')
+  )
+}
+
+/**
+ * 残り秒数のOGP画像を作成
+ * @param name アイドル名
+ * @param seconds 秒数
+ * @param color イメージカラー
+ * @returns OGP画像URL
+ */
+function createCountdownOgpImageUrl(
+  name: string,
+  seconds: number,
+  color: string
+) {
+  return cloudinary.v2.url('idol-birthday-countdown/countdown.png', {
+    secure: true,
+    sign_url: true,
+    transformation: [
+      {
+        variables: [
+          ['$idol', `!${encodeForCloudinary(name)}!`],
+          ['$seconds', `!${seconds}!`],
+          ['$color', `!rgb:${color.replace('#', '')}!`]
+        ]
+      },
+      {
+        transformation: ['idol-countdown']
+      }
+    ]
+  })
+}
+
+/**
+ * お誕生日のOGP画像を作成
+ * @param name アイドル名
+ * @param color イメージカラー
+ * @returns OGP画像URL
+ */
+function createHpbOgbImageUrl(name: string, color: string) {
+  return cloudinary.v2.url('idol-birthday-countdown/hpb.png', {
+    secure: true,
+    sign_url: true,
+    transformation: [
+      {
+        variables: [
+          ['$idol', `!${encodeForCloudinary(name)}!`],
+          ['$color', `!rgb:${color.replace('#', '')}!`]
+        ]
+      },
+      {
+        transformation: ['idol-hpb']
+      }
+    ]
+  })
+}
+
+/**
  * OGP画像URLを作成
  * @param idol アイドルデータ
  * @param timestamp タイムスタンプ
@@ -32,8 +97,11 @@ export function createOgpImageUrl(
   const jstDate = createJstDate(timestamp)
   const seconds = calcSecondsToBirthday(jstDate, idol.birth)
 
-  console.log('[OGP] 残り秒数: ' + seconds)
+  // OGP画像を生成
+  const ogpImageUrl =
+    seconds <= 0
+      ? createHpbOgbImageUrl(idol.name, idol.color.hex)
+      : createCountdownOgpImageUrl(idol.name, seconds, idol.color.hex)
 
-  // TODO: OGP画像を生成
-  return ''
+  return ogpImageUrl
 }
