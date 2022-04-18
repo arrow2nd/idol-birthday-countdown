c9da93e728feea2d86cfda97bb3bf500adf97e4a
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-04-18 22:26:21 +0900

    feat: カウントダウンの停止を実装

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -16,12 +16,21 @@ const Home = ({
   ogpImageUrl,
   dateHash
 }: ServerSideProps): JSX.Element => {
+  const [prevDate] = useState(getNowJstDate().getDate())
   const [seconds, setSeconds] = useState<number>()
 
-  // TODO: 誕生日になったらストップする
-  useInterval(() => {
+  const { state, stop } = useInterval(() => {
+    // ハッシュを再計算するために日付が変わったらリロード
+    if (getNowJstDate().getDate() !== prevDate) {
+      window.location.reload()
+    }
+
     setSeconds(calcSecondsToBirthday(getNowJstDate(), idol.birth))
   })
+
+  if (state === 'running' && typeof seconds !== 'undefined' && seconds <= 0) {
+    stop()
+  }
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen select-none">
--- hooks/useInterval.ts
+++ hooks/useInterval.ts
@@ -1,15 +1,50 @@
-import { useEffect, useRef } from 'react'
+import { useCallback, useEffect, useReducer, useRef } from 'react'
 
-export const useInterval = (callback: () => void) => {
+type Action = 'start' | 'stop'
+type State = 'running' | 'stopped'
+
+type Control = {
+  state: State
+  stop: () => void
+}
+
+const reducer = (state: State, action: Action): State => {
+  switch (action) {
+    case 'start':
+      return 'running'
+    case 'stop':
+      return 'stopped'
+    default:
+      return state
+  }
+}
+
+export const useInterval = (callback: () => void): Control => {
+  const [state, dispatch] = useReducer(reducer, 'stopped')
   const callbackRef = useRef<() => void>(callback)
+
+  const stop = useCallback(() => dispatch('stop'), [])
+
+  useEffect(() => dispatch('start'), [])
 
   useEffect(() => {
     callbackRef.current = callback
   }, [callback])
 
   useEffect(() => {
-    const id = setInterval(() => callbackRef.current(), 1000)
-    callbackRef.current()
-    return () => clearInterval(id)
-  }, [])
+    let timerId: NodeJS.Timer | undefined
+
+    if (state === 'running') {
+      timerId = setInterval(() => callbackRef.current(), 1000)
+      callbackRef.current()
+    } else if (timerId) {
+      clearInterval(timerId)
+    }
+
+    return () => {
+      timerId && clearInterval(timerId)
+    }
+  }, [state])
+
+  return { state, stop }
 }
